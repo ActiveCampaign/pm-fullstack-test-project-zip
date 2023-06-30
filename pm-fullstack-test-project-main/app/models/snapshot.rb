0ef683c7c010b7689880026ed459bf8d4f119baa
Viktor Zaremba <c_vzaremba@groupon.com>
2023-06-30 12:29:15 -0500

    Modify the Snapshot model to create Snapshot data from messages fetched from Postmark

--- pm-fullstack-test-project-main/app/models/snapshot.rb
+++ pm-fullstack-test-project-main/app/models/snapshot.rb
@@ -6,8 +6,31 @@ class Snapshot < ApplicationRecord
   def self.take
     connection = Postmark::ApiClient.new(Rails.application.config.x.postmark.api_token)
 
-    # See usage docs at https://github.com/wildbit/postmark-gem
-    data = {}
+    # Fetch outbound messages
+    messages = connection.get_messages(count: 500) # Adjust the count based on your requirements
+
+    # Transform the messages into nodes and links
+    nodes = []
+    links = []
+    messages.each do |message|
+      from_address = extract_address(message[:from])
+      to_emails = message[:to]
+      to_emails.each do |email|
+        to_address = email["Name"]
+        nodes << { id: from_address }
+        nodes << { id: to_address }
+        links << { source: from_address, target: to_address }
+      end
+    end
+
+    # Ensure unique nodes
+    nodes.uniq! { |node| node[:id] }
+
+    # Create data for Snapshot
+    data = {
+      nodes: nodes,
+      links: links
+    }
 
     Snapshot.new(data: data)
   end
